16f92f18f4570bd8cc1c50e2436350cd559aa351
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-04 14:34:11 +0530

    Accept a number from the user and check whether it  is an Armstrong Number.

---
+++ LabWork/Iteration_Lab_4th_June/armstrongNumber.py
@@ -0,0 +1,18 @@
+
+# Accept a number from the user and check whether it is an Armstrong Number.
+user_input=int(input("Input:"))
+og=user_input
+power=len(str(user_input))
+sum=0
+while(user_input>0):
+    num=user_input%10
+    sum+=num**power
+    user_input=user_input//10
+    
+
+if(sum==user_input):
+    print("Output:")
+    print(og,"is an Armstrong Number")
+else:   
+    print("Output:")
+    print(og,"is not an Armstrong Number") 
